ce4b79df151326d709b12511e90bfca49adfd743
Teona Maghradze <maghradzeteo@gmail.com>
2023-12-11 12:50:41 +0400

    add parallax and animations of it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./app.scss";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 
 function App() {
   return (
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
-      <section>Parallax</section>
+      <section>
+        <Parallax type="services" />
+      </section>
       <section id="Services">Services</section>
-      <section id="Portfolio">Parralax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>Portfolio2</section>
       <section>Portfolio3</section>
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,85 @@
+import "./Hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVariants = {
+  initial: {
+    x: 0,
+  },
+
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}>TEONA MAGHRADZE</motion.h2>
+          <motion.h1 variants={textVariants}>Web Developer</motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>
+              See the latest works
+            </motion.button>
+            <motion.button variants={textVariants}>Contact me</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            src="/scroll.png"
+            alt=""
+            animate="scrollButton"
+          />
+        </motion.div>
+      </div>
+      <motion.div
+        className="slidingTxtContainer"
+        variants={sliderVariants}
+        initial="initial"
+        animate="animate"
+      >
+        Writer content creator infuencer
+      </motion.div>
+
+      <div className="imageContainer">
+        <img src="/hero.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
